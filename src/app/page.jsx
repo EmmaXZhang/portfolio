2fe20f069808ab5f10a5ce2017902b89f7811530
EmmaZhang <emmazxd@gmail.com>
2024-04-29 16:50:21 +1000

    change homepage link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,11 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import StarsCanvas from "@/components/StarBackground";
+import Link from "next/link";
 
 const Homepage = () => {
+  const text = "Hi I'm Emma";
+
   return (
     <motion.div
       className="h-full"
@@ -21,7 +24,23 @@ const Homepage = () => {
         {/* text */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* title */}
-          <h1 className="text-4xl md:text-6xl font-bold">Hi I&apos;m Emma</h1>
+          <div className="text-4xl md:text-6xl font-bold">
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+          </div>
+
           {/* description */}
           <p className="md:text-xl">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -30,12 +49,16 @@ const Homepage = () => {
             obcaecati, cum, accusamus sed dolores ullam fuga.
           </p>
           <div className="w-full flex gap-4">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            <Link
+              href="/portfolio"
+              key="portfolio"
+              className="p-4 rounded-lg ring-1 ring-black bg-black text-white"
+            >
               View My Work
-            </button>
-            <button className="p-4 rounded-lg ring-1 ring-black">
+            </Link>
+            <Link href="/contact" className="p-4 rounded-lg ring-1 ring-black">
               Contact Me
-            </button>
+            </Link>
           </div>
         </div>
       </div>
