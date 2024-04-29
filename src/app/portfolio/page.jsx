139c4c666f69886feb6ad0fc04798a062b69e908
EmmaZhang <emmazxd@gmail.com>
2024-04-29 20:23:57 +1000

    fix bug

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,5 +1,10 @@
 "use client";
-import { motion, useTransform, useScroll } from "framer-motion";
+import {
+  motion,
+  useTransform,
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
 import { useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -47,49 +52,59 @@ const PortfolioPage = () => {
   const ref = useRef();
   const { scrollYProgress } = useScroll({ target: ref });
   // [0,1]->y, [0,1]->x
-  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-78%"]);
+
+  console.log("scrollYProgress: ", scrollYProgress);
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log("Page scroll: ", latest);
+  });
 
   return (
-    <motion.div
-      className="h-full"
-      initial={{ y: "-200vh" }}
-      animate={{ y: "0%" }}
-      transition={{ duration: 1 }}
-    >
-      <div className="h-[600vh] relative" ref={ref}>
-        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
-          My Works
-        </div>
+    <div style={{ height: "100vh" }}>
+      <motion.div
+        className="h-full relative"
+        initial={{ y: "-200vh" }}
+        animate={{ y: "0%" }}
+        transition={{ duration: 1 }}
+      >
+        <div className="h-[600vh]" ref={ref}>
+          <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+            My Works
+          </div>
 
-        <div className="sticky top-0 flex h-screen gap-4 items-center">
-          <motion.div style={{ x }} className="flex">
-            {items.map((item) => (
+          <div className="sticky top-0 flex h-screen gap-4 items-center">
+            <motion.div style={{ x }} className="flex">
               <div
-                className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
-                key={item.id}
-              >
-                <div className="flex flex-col gap-6 text-white items-center justify-center ">
-                  <h1 className="font-bold mb-10 text-xl md:text-4xl lg:text-6xl xl:text=8xl">
-                    {item.title}
-                  </h1>
-                  <div className="relative w-80 h-56 md:w-96 md:h-[504px] lg:w-[500px] l:h-[650px] xl:w-[600px] xl:h-[700px]">
-                    <Image src={item.img} alt="" fill />
+                className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300`}
+              ></div>
+              {items.map((item) => (
+                <div
+                  className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
+                  key={item.id}
+                >
+                  <div className="flex flex-col gap-6 text-white items-center justify-center ">
+                    <h1 className="font-bold mb-10 text-xl md:text-4xl lg:text-6xl xl:text=8xl">
+                      {item.title}
+                    </h1>
+                    <div className="relative w-80 h-56 md:w-96 md:h-[504px] lg:w-[500px] l:h-[650px] xl:w-[600px] xl:h-[700px]">
+                      <Image src={item.img} alt="" fill />
+                    </div>
+                    <p className="w-80 md:96 lg:w-[500px] lg:text-lg xl:w-[600px]">
+                      {item.description}
+                    </p>
+                    <Link href={item.link} className="flex justify-end">
+                      <button className="p-2 text-sm md:p-4 md:text-md lg:p-3 lg:text-lg text-gray-500 font-semibold m-4 rounded bg-white">
+                        See Demo
+                      </button>
+                    </Link>
                   </div>
-                  <p className="w-80 md:96 lg:w-[500px] lg:text-lg xl:w-[600px]">
-                    {item.description}
-                  </p>
-                  <Link href={item.link} className="flex justify-end">
-                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-3 lg:text-lg text-gray-500 font-semibold m-4 rounded bg-white">
-                      See Demo
-                    </button>
-                  </Link>
                 </div>
-              </div>
-            ))}
-          </motion.div>
+              ))}
+            </motion.div>
+          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 };
 
